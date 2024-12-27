8a3950003516f91275b21012f109fffc99537a5c
naolatomsa <naolatomsa4@gmail.com>
2024-12-27 16:54:11 +0300

    outlier detection

--- scripts/dataCleaning.py
+++ scripts/dataCleaning.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 def missing_percentage(data, thresholds):
     missing_percentage = (data.isnull().sum() / len(data)) * 100
     missing_columns_percentage = missing_percentage[missing_percentage > thresholds]
@@ -23,13 +26,29 @@ def drop_column(data, column_to_drop):
     return data;
 
 
-def identify_outliers(data, column):
-    Q1 = data[column].quantile(0.25)
-    Q3 = data[column].quantile(0.75)
-    IQR = Q3 - Q1
-    lower_bound = Q1 - 1.5 * IQR
-    upper_bound = Q3 + 1.5 * IQR
-    return data[(data[column] < lower_bound) | (data[column] > upper_bound)]
+def identify_outliers(data, columns):
+    outliers = {}
+
+    for column in columns:
+        Q1 = data[column].quantile(0.25)
+        Q3 = data[column].quantile(0.75)
+        IQR = Q3 - Q1
+        lower_bound = Q1 - 1.5 * IQR
+        upper_bound = Q3 + 1.5 * IQR
+
+        # Identify outliers
+        outliers[column] = data[(data[column] < lower_bound) | (data[column] > upper_bound)]
+
+        plt.figure(figsize=(8, 6))
+        sns.boxplot(x=data[column], color='skyblue')
+        plt.axvline(lower_bound, color='red', linestyle='--', label='Lower Bound')
+        plt.axvline(upper_bound, color='red', linestyle='--', label='Upper Bound')
+        plt.title(f"Box Plot for {column} with Outlier Bounds")
+        plt.xlabel(column)
+        plt.legend()
+        plt.show()
+
+    return outliers
 
 # Cap and floor outliers for all numeric columns
 def cap_outliers(data, column):
